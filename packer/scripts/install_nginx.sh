e59b1509c78d9787b83b4ed76b2e05d761f4008a
Vladyslav-Stepanenko <vlad.stepanenko@alpacked.io>
2022-02-16 19:15:38 +0200

    Update script [ci skip]

--- packer/scripts/install_nginx.sh
+++ packer/scripts/install_nginx.sh
@@ -6,12 +6,5 @@ sudo ufw allow 'Nginx HTTP'
 sudo ufw reload
 sudo ufw status
 systemctl status nginx
-
 git clone https://github.com/Vladyslav-Stepanenko/nginx-test.git /home/ubuntu/nginx-test
-sudo mv /etc/nginx/nginx.conf /home/ubuntu/nginx-test/nginx.conf
-
-sudo mkdir -p /home/ubuntu/www
-sudo mv /home/ubuntu/nginx-test/index.html /home/ubuntu/www/index.html
-
-sudo systemctl restart nginx
-systemctl status nginx
+sudo cp /home/ubuntu/nginx-test/index.html /var/www/html/index.html
